a1300ee15dacf1028086f2ea014e1234da93f73d
Agaphon1888 <egorov2000@inbox.ru>
2024-10-26 12:52:40 +0500

    # Домашнее задание по теме "Введение в функциональное программирование". # Задача "Вызов разом".

---
+++ module_9_1.py
@@ -0,0 +1,13 @@
+# Домашнее задание по теме "Введение в функциональное программирование".
+# Задача "Вызов разом".
+
+def apply_all_func(int_list, *functions):
+    results = {}  # создаем пустой словарь для результатов
+    for func in functions:  # перебираем все функции
+        results[func.__name__] = func(int_list)  # вызываем функцию и сохраняем результат
+    return results  # возвращаем словарь с результатами
+
+
+# Примеры использования функции
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))

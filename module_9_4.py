fe265f4f5c7c8cb6258920275a72934ae4fa1c14
Agaphon1888 <egorov2000@inbox.ru>
2024-10-27 19:55:43 +0500

    Домашнее задание по теме "Создание функций на лету". Задача "Функциональное разнообразие".

---
+++ module_9_4.py
@@ -0,0 +1,43 @@
+# Домашнее задание по теме "Создание функций на лету".
+# Задача "Функциональное разнообразие".
+
+from random import choice
+
+# Задача 1: Функциональное разнообразие
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+# Определяем lambda-функцию
+result = list(map(lambda x, y: x == y, first, second))
+print(result)  # Вывод: [False, True, True, False, False, False, False, False, True, False, False, False, False, False]
+
+
+# Задача 2: Замыкание
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as f:
+            for data in data_set:
+                f.write(str(data) + '\n')
+
+    return write_everything
+
+
+# Пример использования
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# Задача 3: Метод __call__
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+# Пример использования
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())  # Вывод случайного слова
+print(first_ball())  # Вывод случайного слова
+print(first_ball())  # Вывод случайного слова
